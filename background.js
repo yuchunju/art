2fb1d3714e66959eb1fd8c4254bb8b2b656bc1c5
Ambrala <ambralaju@gmail.com>
2020-08-09 01:06:06 +0200

    Add files via upload

--- background.js
+++ background.js
@@ -1,7 +1,5 @@
-
-var canvas;
-let movers = [];
-let liquid;
+let system;
+ 
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -11,12 +9,11 @@ function windowResized() {
 
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0,0);
   canvas.style('z-index','-1');
-  reset();
-  // Create liquid object
-  liquid = new Liquid(0, height / 2, width, height / 2, 0.1);
+  system = new ParticleSystem(createVector(width / 3 , 50));
+  
 }
 
 
@@ -32,117 +29,58 @@ function setup() {
 //  rotete(angle);
 //  rectX(0,0,150,150);
 //  angle +=0.07;
-  liquid.display();
-
-  for (let i = 0; i < movers.length; i++) {
-
-    // Is the Mover in the liquid?
-    if (liquid.contains(movers[i])) {
-      // Calculate drag force
-      let dragForce = liquid.calculateDrag(movers[i]);
-      // Apply drag force to Mover
-      movers[i].applyForce(dragForce);
-    }
-
-    // Gravity is scaled by mass here!
-    let gravity = createVector(0, 0.1 * movers[i].mass);
-    // Apply gravity
-    movers[i].applyForce(gravity);
-
-    // Update and display
-    movers[i].update();
-    movers[i].display();
-    movers[i].checkEdges();
-  }
- 
- 
- }
-
-function mousePressed() {
-  reset();
+  system.addParticle();
+  system.run();
 }
 
-// Restart all the Mover objects randomly
-function reset() {
-  for (let i = 0; i < 9; i++) {
-    movers[i] = new Mover(random(0.5, 3), 40 + i * 70, 0);
-  }
-}
-
-let Liquid = function(x, y, w, h, c) {
-  this.x = x;
-  this.y = y;
-  this.w = w;
-  this.h = h;
-  this.c = c;
+// A simple Particle class
+let Particle = function(position) {
+  this.acceleration = createVector(0, 0.05);
+  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  this.position = position.copy();
+  this.lifespan = 255;
 };
 
-// Is the Mover in the Liquid?
-Liquid.prototype.contains = function(m) {
-  let l = m.position;
-  return l.x > this.x && l.x < this.x + this.w &&
-         l.y > this.y && l.y < this.y + this.h;
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
 };
 
-// Calculate drag force
-Liquid.prototype.calculateDrag = function(m) {
-  // Magnitude is coefficient * speed squared
-  let speed = m.velocity.mag();
-  let dragMagnitude = this.c * speed * speed;
-
-  // Direction is inverse of velocity
-  let dragForce = m.velocity.copy();
-  dragForce.mult(-1);
-
-  // Scale according to magnitude
-  // dragForce.setMag(dragMagnitude);
-  dragForce.normalize();
-  dragForce.mult(dragMagnitude);
-  return dragForce;
-};
-
-Liquid.prototype.display = function() {
-  noStroke();
-  fill(50);
-  rect(this.x, this.y, this.w, this.h);
-};
-
-function Mover(m, x, y) {
-  this.mass = m;
-  this.position = createVector(x, y);
-  this.velocity = createVector(0, 0);
-  this.acceleration = createVector(0, 0);
-}
-
-// Newton's 2nd law: F = M * A
-// or A = F / M
-Mover.prototype.applyForce = function(force) {
-  let f = p5.Vector.div(force, this.mass);
-  this.acceleration.add(f);
-};
-
-Mover.prototype.update = function() {
-  // Velocity changes according to acceleration
+// Method to update position
+Particle.prototype.update = function(){
   this.velocity.add(this.acceleration);
-  // position changes by velocity
   this.position.add(this.velocity);
-  // We must clear acceleration each frame
-  this.acceleration.mult(0);
+  this.lifespan -= 0;
 };
 
-Mover.prototype.display = function() {
-  stroke(0);
-  strokeWeight(2);
-  fill(255,127);
-  ellipse(this.position.x, this.position.y, this.mass * 16, this.mass * 16);
+// Method to display
+Particle.prototype.display = function() {
+  stroke(250, this.lifespan);
+  strokeWeight(10);
+  fill(250, this.lifespan);
+  ellipse(this.position.x, this.position.y, 12, 12);
 };
 
-// Bounce off bottom of window
-Mover.prototype.checkEdges = function() {
-  if (this.position.y > (height - this.mass * 8)) {
-    // A little dampening when hitting the bottom
-    this.velocity.y *= -0.9;
-    this.position.y = (height - this.mass * 8);
+// Is the particle still useful?
+Particle.prototype.isDead = function(){
+  return this.lifespan < 0;
+};
+
+let ParticleSystem = function(position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function() {
+  this.particles.push(new Particle(this.origin));
+};
+
+ParticleSystem.prototype.run = function() {
+  for (let i = this.particles.length-1; i >= 0; i--) {
+    let p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
   }
 };
-
