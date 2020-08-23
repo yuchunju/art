ece4d15dbc4b2b97c553b978ff2a25c5fc394a34
Ambrala <ambralaju@gmail.com>
2020-08-23 17:55:22 +0200

    Add files via upload

--- lang.js
+++ lang.js
@@ -1,4 +1,21 @@
 // JavaScript Document
+function setCookie(name,value)
+{
+    var Days = 30;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days*24*60*60*1000);
+    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+}
+ 
+//获取cookie
+function getCookie(name)
+{
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+    if(arr=document.cookie.match(reg))
+    return unescape(arr[2]);
+    else
+    return null;
+}
 
 var LanguageList = {
   "EN" : "English",
@@ -46,6 +63,9 @@ function initialize() {
     });
     
   loadsLanguage("EN");
+  var lan = getCookie('LanguageList');
+
+
 }
 
 function loadsLanguage(lang){
