52346bd2695b007138712f90e5377345327478e7
Ambrala <ambralaju@gmail.com>
2020-09-07 15:56:17 +0200

    Add files via upload

--- background.js
+++ background.js
@@ -1,9 +1,4 @@
-let numBalls = 13;
-let spring = 0.05;
-let gravity = 0.10;
-let friction = -1.0;
-let balls = [];
-let img = []
+let system;
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -11,104 +6,76 @@ function windowResized() {
 }
 
 
-
-
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0,0);
   canvas.style('z-index','-1');
-
-    img = new Image();
-    img.src = "lib/pic/day03.png";
- 
-	
-	
-for (let i = 0; i < numBalls; i++) {
-    balls[i] = new Ball(
-      random(width),
-      random(height),
-      random(50, 90),
-      i,
-      balls
-    );
-  }
-  noStroke();
-  fill(255, 204);
-	
-  }
-
-
-
-
-
-function draw() {
-	background('#99CCFF');
-	
-  balls.forEach(ball => {
-    ball.collide();
-    ball.move();
-    ball.display();
-  });
+  system = new ParticleSystem(createVector(width / 2.5 , 50));
+  
 }
 
-class Ball {
-  constructor(xin, yin, din, idin, oin) {
-    this.x = xin;
-    this.y = yin;
-    this.vx = 0;
-    this.vy = 0;
-    this.diameter = din;
-    this.id = idin;
-    this.others = oin;
-  }
 
-  collide() {
-    for (let i = this.id + 1; i < numBalls; i++) {
-      // console.log(others[i]);
-      let dx = this.others[i].x - this.x;
-      let dy = this.others[i].y - this.y;
-      let distance = sqrt(dx * dx + dy * dy);
-      let minDist = this.others[i].diameter / 2 + this.diameter / 2;
-      //   console.log(distance);
-      //console.log(minDist);
-      if (distance < minDist) {
-        //console.log("2");
-        let angle = atan2(dy, dx);
-        let targetX = this.x + cos(angle) * minDist;
-        let targetY = this.y + sin(angle) * minDist;
-        let ax = (targetX - this.others[i].x) * spring;
-        let ay = (targetY - this.others[i].y) * spring;
-        this.vx -= ax;
-        this.vy -= ay;
-        this.others[i].vx += ax;
-        this.others[i].vy += ay;
-      }
-    }
-  }
 
-  move() {
-    this.vy += gravity;
-    this.x += this.vx;
-    this.y += this.vy;
-    if (this.x + this.diameter / 2 > width) {
-      this.x = width - this.diameter / 2;
-      this.vx *= friction;
-    } else if (this.x - this.diameter / 2 < 0) {
-      this.x = this.diameter / 2;
-      this.vx *= friction;
-    }
-    if (this.y + this.diameter / 2 > height) {
-      this.y = height - this.diameter / 2;
-      this.vy *= friction;
-    } else if (this.y - this.diameter / 2 < 0) {
-      this.y = this.diameter / 2;
-      this.vy *= friction;
-    }
-  }
 
-  display() {
-   drawingContext.drawImage(img,this.x, this.y, this.diameter, this.diameter);
-		
+
+ function draw() {
+ background('#99CCFF');
+  
+  system.addParticle();
+  system.run();
+  
+}
+
+// A simple Particle class
+let Particle = function(position) {
+  this.acceleration = createVector(0, 0.05);
+  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  this.position = position.copy();
+  this.lifespan = 255;
+};
+
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Particle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 0;
+};
+
+// Method to display
+Particle.prototype.display = function() {
+  stroke(250, this.lifespan);
+  strokeWeight(10);
+  fill(250, this.lifespan);
+  ellipse(this.position.x, this.position.y, 12, 12);
+};
+
+// Is the particle still useful?
+Particle.prototype.isDead = function(){
+  return this.lifespan < 0;
+};
+
+let ParticleSystem = function(position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function() {
+  this.particles.push(new Particle(this.origin));
+};
+
+ParticleSystem.prototype.run = function() {
+  for (let i = this.particles.length-1; i >= 0; i--) {
+    let p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
   }
 }
+
 
